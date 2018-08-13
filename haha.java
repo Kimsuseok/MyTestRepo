f72ddc5012d7e3d1d74391a3a991220a372531f0
suseok.kim <suseok.kim@navercorp.com>
2018-08-13 11:14:27 +0900

    Repo2 가 Repo1이 나눠준 서브브랜치에서 Commit1

--- haha.java
+++ haha.java
@@ -14,7 +14,6 @@ Repo1 에서 수정1
 Repo1 에서 수정2
 Repo1 에서 수정3
 
-<<<<<<< HEAD
 Repo3에서 rebase2 브랜치 나눈이후 작업 1
 
 Repo3에서 rebase2 브랜치 나눈이후 작업 2
@@ -29,3 +28,4 @@ Repo1 에서 서브 브랜치 나눈 이후에 수정6 (commit4)
 
 Repo1 에서 서브 브랜치 나눈 이후에 수정7 (commit5)
 
+Repo2가 Repo1이 나눠준 subbrnach에서 수정 (commit1)
